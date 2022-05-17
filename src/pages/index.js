13759c8a5d3d079228e32e3cf91424c18920d06f
Pavel Tkachuk <p.s.tkachuk@yandex.ru>
2022-05-17 11:47:46 +0300

    собрать проект webpack'ом

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 //импорты
+import './index.css';
 import { Card }  from '../components/Card.js';
 import { FormValidator } from '../components/FormValidator.js';
 import { Section } from '../components/Section.js';
